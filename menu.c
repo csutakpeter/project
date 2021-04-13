98d6609ca2832ce61c13d0cc58b71347ea10b0a6
BHunor <burszanhunor@gmail.com>
2021-04-13 10:59:36 +0300

    Almost done

--- menu.c
+++ menu.c
@@ -24,29 +24,29 @@ void menu(BOARD** pBoards, int* pNumberOfBoards, USER** pUsers, int* pNumberOfUs
                 //Adatok torlese a konzolbol ( CLion terminalban nem mukodik! )
             case 1 : system("cls"); infoBox(); break;
                 //uj felhasznalo letrehozasa
-            case 2 : action1(pUsers, pNumberOfUsers); break;
+            case 2 : action1(&(*pUsers), pNumberOfUsers); break;
                 //osszes felhasznalo kiirasa
             case 3 : action2((*pUsers), (*pNumberOfUsers)); break;
                 //Uj tabla letrehozasa
-            case 4 : action3(pBoards, pNumberOfBoards); break;
+            case 4 : action3(&(*pBoards), pNumberOfBoards); break;
                 // felhasznalo hozzarendelese tablahoz
-            case 5 : action4(pBoards, (*pNumberOfBoards), pUsers, (*pNumberOfUsers)); break;
+            case 5 : action4(&(*pBoards), (*pNumberOfBoards), &(*pUsers), (*pNumberOfUsers)); break;
                 // konkret tabla (nev szerint) adatainak kiirasa
             case 6 : action5((*pBoards), (*pNumberOfBoards)); break;
                 // kartya hozzaadasa tablahoz
-            case 7 : action6(pBoards, (*pNumberOfBoards)); break;
+            case 7 : action6(&(*pBoards), (*pNumberOfBoards)); break;
                 // kartya torlese tablabol
-            case 8 : action7(pBoards, (*pNumberOfBoards)); break;
+            case 8 : action7(&(*pBoards), (*pNumberOfBoards)); break;
                 // user hozzaadasa kartyahoz
-            case 9 : action8(pBoards, (*pNumberOfBoards)); break;
+            case 9 : action8(&(*pBoards), (*pNumberOfBoards)); break;
                 // kartyan dolgozott felhasznalok lekerdezese
             case 10 : action9((*pBoards), (*pNumberOfBoards)); break;
                 // kartya statuszanak megvaltoztatasa
-            case 11 : action10(pBoards, (*pNumberOfBoards)); break;
+            case 11 : action10(&(*pBoards), (*pNumberOfBoards)); break;
                 // egy kartya statuszanak lekerdezese
             case 12 : action11((*pBoards), (*pNumberOfBoards)); break;
                 // kartya adatainak valtoztatasa
-            case 13 : action12(pBoards, (*pNumberOfBoards)); break;
+            case 13 : action12(&(*pBoards), (*pNumberOfBoards)); break;
                 // barmilyen statuszu kartya megjelenitese
             case 14 : action13((*pBoards), (*pNumberOfBoards)); break;
                 //ki listazza a boardokat
@@ -88,9 +88,6 @@ void freeEverything(BOARD** pBoards, int numberOfBoards, USER** pUsers, int numb
 
     for (int i = 0; i < numberOfUsers; ++i) {
         freeUser(&(*pUsers)[i]);
-        printf("\nXD\n");
     }
-    printf("\nUser megsemmisitve!");
-
     free((*pUsers));
 }
